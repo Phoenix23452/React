fa81c2d060965a6830e768cee81901201c4387bb
Muhammad Tuaha <1phoenix23452@gmail.com>
2023-10-21 14:03:07 +0500

    Update date logic

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -4,9 +4,20 @@ import '../styles/ExpenseItem.css'
 
 
 export default function ExpenseItem(props) {
-  return (
+  const month = props.expenses.date.toLocaleString('en-US', {'month': 'long'})
+  const day = props.expenses.date.toLocaleString('en-US', {day: '2-digit'})
+  const year = props.expenses.date.getFullYear()
+
+
+  
+    return (
     <div className='expense-item'>
-      <div>{props.expenses.date.toISOString()}</div>
+      <div>
+        <div>{month}</div>
+        <div>{year}</div>
+        <div>{day}</div>
+
+      </div>
       <div className='expense-item__description'>
         <h1>{props.expenses.title}</h1>
         <div className='expense-item__price'>$ {props.expenses.amount}</div>
