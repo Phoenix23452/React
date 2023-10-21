1c5df8c5b4967cad4e6fc83ca0eb6f9fcb34e240
Muhammad Tuaha <1phoenix23452@gmail.com>
2023-10-21 14:19:05 +0500

    Creating Date Component and styling it and also add a new style file for Date Component

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,27 +1,21 @@
 import React from 'react'
 import '../styles/ExpenseItem.css'
+import ExpenseDate from './ExpenseDate';
 
 
 
 export default function ExpenseItem(props) {
-  const month = props.expenses.date.toLocaleString('en-US', {'month': 'long'})
-  const day = props.expenses.date.toLocaleString('en-US', {day: '2-digit'})
-  const year = props.expenses.date.getFullYear()
 
 
-  
+
+
     return (
-    <div className='expense-item'>
-      <div>
-        <div>{month}</div>
-        <div>{year}</div>
-        <div>{day}</div>
-
-      </div>
-      <div className='expense-item__description'>
-        <h1>{props.expenses.title}</h1>
-        <div className='expense-item__price'>$ {props.expenses.amount}</div>
-      </div>
-    </div>
-  )
+        <div className='expense-item'>
+            <ExpenseDate date={props.expenses.date}/>
+            <div className='expense-item__description'>
+                <h1>{props.expenses.title}</h1>
+                <div className='expense-item__price'>$ {props.expenses.amount}</div>
+            </div>
+        </div>
+    )
 }
